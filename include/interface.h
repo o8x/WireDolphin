ce58378186444d638b1df0cae6509ca42e713993
alex <rwrx@foxmail.com>
2024-08-02 19:58:55 +0800

    feat: stats add time

--- include/interface.h
+++ include/interface.h
@@ -7,4 +7,4 @@ pcap_t* open_interface(const char* device, char* ebuf);
 string get_dlt_name(pcap_t*);
 string get_dlt_desc(pcap_t*);
 
-void print_stat_info(pcap_t* inet, int packet_num);
+void print_stat_info(pcap_t*, const size_t, chrono::time_point<chrono::steady_clock>);
