67a43776ac57e103a9408d301c9197fa69f9c20c
alex <rwrx@foxmail.com>
2024-08-13 12:25:47 +0800

    feat: code sign for .app

--- hack/build.sh
+++ hack/build.sh
@@ -11,7 +11,16 @@ cmake -G Ninja -S $(pwd) -B ${CMAKE_BUILD_DIR} \
 cmake --build ${CMAKE_BUILD_DIR} --target all -j `nproc`
 
 cd ${CMAKE_BUILD_DIR}
-macdeployqt6 *.app -dmg
+APP_NAME=$(cd *.app ; echo $(basename $(pwd)))
+
+# 生成证书
+# https://developer.apple.com/help/account/create-certificates/create-a-certificate-signing-request
+# 不签名无法在开启了 SIP 的系统中运行
+codesign -s "WireDolphin" $APP_NAME || true
+codesign --display --verbose=4 $APP_NAME
+
+csrutil status
+macdeployqt6 $APP_NAME -dmg
 cd ..
 
 mv ${CMAKE_BUILD_DIR}/*.dmg .
